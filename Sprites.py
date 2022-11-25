d395c5056d2dc14a1d8c8b39018935c9583602ee
luisaabibi <lubiabibi@gmail.com>
2022-11-25 15:29:59 -0300

    adicionando o páasaro

---
+++ Sprites.py
@@ -0,0 +1,8 @@
+import pygame
+class bird(pygame.sprite.Sprite):
+    def __init__(self, img, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
--- jogo_v1.py
+++ jogo_v1.py
@@ -2,21 +2,40 @@ import random
 from tkinter.tix import Tree
 import pygame
 import time
-
+from Sprites import bird 
 pygame.init()
 
 # ----- Gera tela principal
-WIDTH = 360
+WIDTH = 370
 HEIGHT = 640
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flappy Bird')
+
 
 # ----- Inicia estruturas de dados
 game = True
 
 # ----- Inicia assets
-image = pygame.image.load('Assets/img/background.png').convert()
-image = pygame.transform.scale(image, (360,640))
+image = pygame.image.load('Assets/img/background.png').convert_alpha()
+image = pygame.transform.scale(image, (WIDTH, HEIGHT))
+
+bird_img = pygame.image.load("assets/img/bird.png").convert_alpha()
+bird_img = pygame.transform.scale(bird_img, (200, 200))
+fox = bird(bird_img, 150, 295)
+i = 0
+
+# Define nome da janela e icone
+pygame.display.set_caption("Jumpy Fox")
+icone = pygame.image.load("./Assets/img/bird.png")
+pygame.display.set_icon(icone)
+
+# Define loop principal, posicao inicial, pontos iniciais e carrega fonte
+game = True
+jogo = True
+pontos = 0
+posicaox = 0
+fonte = pygame.font.SysFont("comicsans", 30, True)
+
 
 # ===== Loop principal =====
 while game:
@@ -30,7 +49,9 @@ while game:
     window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(image, (10, 10))
 
+
     # ----- Atualiza estado do jogo
+    
     pygame.display.update()
 
 pygame.quit()
