481ffa2841fc654722868744b66deb5b03df9c49
davidfc1 <davidfc@al.insper.edu.br>
2022-11-27 20:41:14 -0300

    Fundo se mexendo e pasaro

--- Sprites.py
+++ Sprites.py
@@ -1,8 +1,30 @@
 import pygame
-class bird(pygame.sprite.Sprite):
-    def __init__(self, img, x, y):
+from pygame.locals import *
+
+
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = img
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.center = [x, y]
+
+    def update(self):
+
+        #handle the animation
+        self.counter += 1
+        flap_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
--- jogo_v2.py
+++ jogo_v2.py
@@ -1,18 +1,56 @@
-import random 
-import sys
 import pygame
-from pygame.locals import * 
+from pygame.locals import *
+from Sprites import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 864
+screen_height = 936
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flapper Birb')
 
 
-#GLOBAL VARIABLES 
+#Variaveis do jogo
+ground_scroll = 0
+scroll_speed = 4
 
-FPS = 32
-SCREENWIDTH = 289
-SCREENHEIGHT = 511
-SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-GROUNDY = SCREENHEIGHT*0.8
-GAME_SPRITES = {}
-GAME_SOUNDS = {}
-PLAYER = 'resources\SPRITES\\bird.png'
-BACKGROUND = 'resources\SPRITES\\bg.jpeg'
-PIPE = 'resources\SPRITES\pipe.png '
+
+bg = pygame.image.load('img/bg.png')
+ground_img = pygame.image.load('img/ground.png')
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    #Fundo
+    screen.blit(bg, (0,0))
+
+    bird_group.draw(screen)
+    bird_group.update()
+
+    #Fundo se mexendo
+    screen.blit(ground_img, (ground_scroll, 768))
+    ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
