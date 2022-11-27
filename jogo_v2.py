fa657d391a5476fb98178c617ee4d65dba9b54bd
davidfc1 <davidfc@al.insper.edu.br>
2022-11-27 20:28:43 -0300

    aaaa

---
+++ jogo_v2.py
@@ -0,0 +1,18 @@
+import random 
+import sys
+import pygame
+from pygame.locals import * 
+
+
+#GLOBAL VARIABLES 
+
+FPS = 32
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+GROUNDY = SCREENHEIGHT*0.8
+GAME_SPRITES = {}
+GAME_SOUNDS = {}
+PLAYER = 'resources\SPRITES\\bird.png'
+BACKGROUND = 'resources\SPRITES\\bg.jpeg'
+PIPE = 'resources\SPRITES\pipe.png '
