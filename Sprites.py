37bcdd5769c03527c38e0e216bace9b8c68831c0
Bruno0nishi <brunoo2@al.insper.edu.br>
2022-11-27 22:05:13 -0300

    gravidade(passaro bugado)

--- Sprites.py
+++ Sprites.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.locals import *
 
-
+flying = False
+game_over = False
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -15,16 +16,39 @@ class Bird(pygame.sprite.Sprite):
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
 
     def update(self):
 
-        #handle the animation
-        self.counter += 1
-        flap_cooldown = 5
+        if flying == True:
+            #Gravidade
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
 
-        if self.counter > flap_cooldown:
-            self.counter = 0
-            self.index += 1
-            if self.index >= len(self.images):
-                self.index = 0
-        self.image = self.images[self.index]
+        if game_over == False:
+            #Pulo
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+            #animação
+            self.counter += 1
+            bate_asa = 5
+
+            if self.counter > bate_asa:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+            self.image = self.images[self.index]
+
+            
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
---
+++ jogo_v3.py
@@ -0,0 +1,70 @@
+
+import pygame
+from pygame.locals import *
+from Sprites import *
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 864
+screen_height = 936
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+
+
+#variaveis do jogo
+mexe_chao = 0
+vel_scroll = 4
+flying = False
+game_over = False
+
+
+bg = pygame.image.load('img/bg.png')
+ground_img = pygame.image.load('img/ground.png')
+
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    #Fundo
+    screen.blit(bg, (0,0))
+
+    bird_group.draw(screen)
+    bird_group.update()
+
+
+    screen.blit(ground_img, (mexe_chao, 768))
+
+    #Contato com o chão
+    if flappy.rect.bottom > 768:
+        game_over = True
+        flying = False
+
+
+    if game_over == False:
+        #Fundo se mexendo
+        mexe_chao -= vel_scroll
+        if abs(mexe_chao) > 35:
+            mexe_chao = 0
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
+
+    pygame.display.update()
+
+pygame.quit()
